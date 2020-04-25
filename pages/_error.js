9d85828e535da097b5d53b18e73f3124d1b96d80
Tiago <tiagosantos.gpsi@gmail.com>
2020-04-25 07:52:06 +0100

    First Commit

---
+++ pages/_error.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Link from "next/link";
+import Router from "next/router";
+
+const errorPage = (props) => {
+  return (
+    <div>
+      <h1>Oops, something went wrong</h1>
+      <p>
+        Try{" "}
+        <Link href="/">
+          <a>going back!</a>
+        </Link>
+        .
+      </p>
+    </div>
+  );
+};
+
+export default errorPage;
